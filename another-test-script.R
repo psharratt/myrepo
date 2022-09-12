06507c3bad9260c49d2378b31182dd317da44392
psharratt <psharratt1@gmail.com>
2022-09-12 19:05:53 +0200

    Create another-test-script.R

---
+++ another-test-script.R
@@ -0,0 +1,7 @@
+pacman::p_load(tidyverse, gapminder)
+data(gapminder)
+head(gapminder)
+
+gapminder_clean <- gapminder %>% 
+  rename(life_exp = lifeExp, gdp_per_cap = gdpPercap) %>% 
+  mutate(gdp = pop * gdp_per_cap)
